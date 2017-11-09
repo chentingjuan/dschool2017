f120f5a98052be808a145513dd0fd05c5842a1e9
cheyuwu <frank890417@gmail.com>
2017-11-10 00:03:23 +0800

    update excel open encoding issue

--- resources/assets/js/components/Data/vue_lazy_table.vue
+++ resources/assets/js/components/Data/vue_lazy_table.vue
@@ -196,11 +196,12 @@ export default {
           // Convert Object to JSON
           var jsonObject = JSON.stringify(items);
 
+          var universalBOM = "\uFEFF";
           var csv = convertToCSV(jsonObject);
 
           var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
 
-          var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+          var blob = new Blob(["\ufeff",csv], { type: 'text/csv;charset=utf-8;' });
           if (navigator.msSaveBlob) { // IE 10+
               navigator.msSaveBlob(blob, exportedFilenmae);
           } else {
