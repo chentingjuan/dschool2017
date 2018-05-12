c9a4cabfbfdc048d43ce00e2992dd4fae67da48d
cheyuwu <frank890417@gmail.com>
2018-05-12 13:07:16 +0800

    update spa

--- commit.sh
+++ commit.sh
@@ -1,5 +1,12 @@
 echo "Enter Commit message:"
 read msg
+echo "Push and Deploy (y/n)? "
+read answer
+
+cd frontend_spa
+npm run build
+cd ..
+
 sh cache.sh
 git add .
 git commit -m "$msg"
@@ -7,8 +14,6 @@ echo ""
 echo "Git Commit complete!"
 echo ""
 
-echo "Push and Deploy (y/n)? "
-read answer
 if echo "$answer" | grep -iq "^y" ;then
     sh deploy.sh
 else
